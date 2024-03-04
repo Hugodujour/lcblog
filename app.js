267b7ad2051835ae206804af8ca88f6c1dc662b7
Hugodujour <hugogreg@live.fr>
2024-03-04 11:07:18 +0100

    ++

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app
-  .use(favicon(__dirname + "/favicon.ico"))
-  .use(morgan("dev"))
-  .use(bodyParser.json());
+app.use(favicon(__dirname + "/favicon.ico")).use(bodyParser.json());
 
 sequelize.initDb();
 
