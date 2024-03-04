e27123f14c8496c1d2e92f2ebea065268214e2e8
Hugodujour <hugogreg@live.fr>
2024-03-04 15:06:24 +0100

    good

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -11,6 +11,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectOptions: {
       useUTC: "Etc/GMT-2",
